2aeced23c4dcca5d467f1b348ff08de461df38a3
silvia0930 <168307977+silvia0930@users.noreply.github.com>
2025-04-14 12:56:18 -0400

    Add files via upload

--- src/data/partners.ts
+++ src/data/partners.ts
@@ -1,1 +1,48 @@
-
+const partners = [
+    {
+      id: 1,
+      name: "Silvia Chen",
+      gender: "Female",
+      major: "Computer Science",
+      course: "CSCI4300, CSCI1730",
+      meetingPreferences: "Online study, Weekend evenings",
+      meetingLocations: "Others",
+      groupSizePreference: "One-on-one, Small group",
+      photo: "/profile.png",
+    },
+    {
+      id: 2,
+      name: "Raini Montalvo",
+      gender: "Male",
+      major: "Computer Science",
+      course: "CSCI1302, CSCI2720",
+      meetingPreferences: "In-person study, Weekend afternoon",
+      meetingLocations: "MLC",
+      groupSizePreference: "One-on-one",
+      photo: "/profile.png"
+    },
+    {
+      id: 3,
+      name: "Asad Khan",
+      gender: "Male",
+      major: "Chemistry",
+      course: "CHEM 2211, CHEM 3211",
+      meetingPreferences: "In-person study, Weekday evening",
+      meetingLocations: "Science Library",
+      groupSizePreference: "Small group",
+      photo: "/profile.png"
+    },
+    {
+      id: 4,
+      name: "Matthew Phanmanee",
+      gender: "Male",
+      major: "Business",
+      course: "BUSN 4000, BUSN 3150",
+      meetingPreferences: "Online study, Weekday morning",
+      meetingLocations: "Tate Student Center",
+      groupSizePreference: "One-on-one, Large group",
+      photo: "/profile.png"
+    }
+  ];
+  
+  export default partners;
