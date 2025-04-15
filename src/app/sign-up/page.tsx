93d293944a7e5dd2685c292d10a295927d2411c3
Rainimontalvo <ram75176@uga.edu>
2025-04-14 20:24:15 -0400

    Update page.tsx

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,1 +1,11 @@
+import SignUpForm from "@/components/SignUpForm";
+
+
+export default function SignUpPage() {
+  return (
+    <main className="flex-grow py-10 bg-gray-100 min-h-screen">
+      <SignUpForm />
+    </main>
+  );
+}
 
