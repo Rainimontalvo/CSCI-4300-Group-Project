fe43b48a9a9154d91fd314815677f55cccda1329
silvia0930 <168307977+silvia0930@users.noreply.github.com>
2025-04-15 00:11:07 -0400

    Add files via upload

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,11 +1,7 @@
-import SignUpForm from "@/components/SignUpForm";
+'use client';
 
+import SignUpForm from '@/components/SignUpForm';
 
 export default function SignUpPage() {
-  return (
-    <main className="flex-grow py-10 bg-gray-100 min-h-screen">
-      <SignUpForm />
-    </main>
-  );
+  return <SignUpForm />;
 }
-
